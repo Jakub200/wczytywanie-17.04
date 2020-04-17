a9390e2f3e55f8c58c84fa2b28ced14f1a48fb78
Jakub200 <62119251+Jakub200@users.noreply.github.com>
2020-04-17 08:44:22 +0200

    Create wczytywanieliczb03.cpp

---
+++ wczytywanieliczb03.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+  ifstream plik2;
+    string a;
+    int i=0;
+ 
+     plik2.open("c:\\liczby.txt");
+     if(plik.good()) 
+       while(!plik2.eof())        
+                         {
+                         plik2 >> t[j++];   
+                         cout << licznik++ << ":" << a;   
+                         cout << endl; 
+                         }
+    for (int j=ilosc-1; j>=0; j--)
+    plik2.close();
+
+    return 0;
+}
+
