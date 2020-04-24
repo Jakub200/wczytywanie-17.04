fd743048cf901f5a0da2be1d67c3a3301bb66f45
Jakub200 <62119251+Jakub200@users.noreply.github.com>
2020-04-24 08:40:56 +0200

    Update wczytywanieliczb03.cpp

--- wczytywanieliczb03.cpp
+++ wczytywanieliczb03.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ using namespace std;
+int main(int argc, char** argv) 
+{
+	string imie="Jan";
+	string nazwisko="Kowalski";
+	string id="1";
+	
+	ofstream wyjscie;
+	 wyjscie.open("project3.json");
+	 
+	 
+	 
+	 plik<<"[ \n {\n \"imie\" : \" "<<imie;
+	  plik<<"[ \n {\n \"nazwisko\" : \" "<<nazwisko
+	   plik<<"[ \n {\n \"id\" : \" "<<id;
+	   
+	plik.close();
+	
+	cout<<endl;
+	system("PAUSE");
+	 return
 
-int main(int argc, char** argv) {
-  ifstream plik2;
-    string a;
-    int i=0;
- 
-     plik2.open("c:\\liczby.txt");
-     if(plik.good()) 
-       while(!plik2.eof())        
-                         {
-                         plik2 >> t[j++];   
-                         cout << licznik++ << ":" << a;   
-                         cout << endl; 
-                         }
-    for (int j=ilosc-1; j>=0; j--)
-    plik2.close();
-
-    return 0;
+	
+	
+	return 0;
 }
-
